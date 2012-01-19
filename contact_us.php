456e0f292105bafabaf82c94bac73b71d2a4f19b
Joshua M Steward <jsteward@virtualproperties.com>
2012-01-19 11:12:22 -0600

    -fixed misspelling in organization's name "Create-Ability, Inc" on the contact page.

--- contact_us.php
+++ contact_us.php
@@ -16,7 +16,7 @@
             <div id="core">
                 <iframe id="googlemaps" width="640" height="480" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="http://maps.google.com/maps?f=q&amp;source=s_q&amp;hl=en&amp;geocode=&amp;q=122+E+Olin+Ave+Suite+255,+Madison,+WI+53713&amp;aq=&amp;sll=43.052379,-89.382083&amp;sspn=0.008342,0.021136&amp;vpsrc=0&amp;ie=UTF8&amp;hq=&amp;hnear=122+E+Olin+Ave,+Madison,+Wisconsin+53713&amp;t=h&amp;view=map&amp;ll=43.052583,-89.381933&amp;spn=0.030105,0.054932&amp;z=14&amp;output=embed">
                 </iframe>
-                <p>Create-Realty, Inc<br />122 E Olin Ave Suite 255<br />Madison, WI 53713</p>
+                <p>Create-Ability, Inc.<br />122 E Olin Ave Suite 255<br />Madison, WI 53713</p>
                 <p>Phone: <b>608-280-0206</b></p>
                 <p>Fax: <b>608-280-0213</b></p>
                 <p>Email: <a href="samw@create-ability.org">samw@create-ability.org</a></p>
